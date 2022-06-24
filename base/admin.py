c654bb6afb4cc4e6ba132b81a621222bc74fc044
mobingh <mobinghanbari111@gmail.com>
2022-06-24 14:36:58 +0430

    the Task page in the admin panel was created

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Task
 # Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'complete', 'created')
