74d3c0fac852d0cf241c6fe654af6d85cd5865b6
mobingh <mobinghanbari111@gmail.com>
2022-06-24 14:32:10 +0430

    the Task model got updated

--- base/models.py
+++ base/models.py
@@ -6,7 +6,7 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
-    create = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
