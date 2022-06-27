4b35bcf504a753b7ff1ee6e7afcf6d53868a4846
mobingh <mobinghanbari111@gmail.com>
2022-06-27 16:26:34 +0430

    the CreateView url was created

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, CreateView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', CreateView.as_view(), name='task-create'),
 ]
