6ed55c49da4e0bc2aa6cfc433753fcdac8cf09b4
mobingh <mobinghanbari111@gmail.com>
2022-07-04 16:52:08 +0430

    display work specific to a user to are not complete

--- base/views.py
+++ base/views.py
@@ -20,6 +20,12 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['tasks'] = context['tasks'].filter(complete=False)
+        return context
+
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     template_name = 'base/task.html'
